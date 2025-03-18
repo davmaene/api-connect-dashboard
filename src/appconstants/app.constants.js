5d6d7e1abc629bc58a438a74a0555d5fdfa67265
davmaene <davidmened@gmail.com>
2025-03-18 18:26:46 +0200

    up --version done

--- src/appconstants/app.constants.js
+++ src/appconstants/app.constants.js
@@ -1,5 +1,5 @@
-// export const baseURL = 'http://127.0.0.1:4001';
-export const baseURL = 'https://backend.mukulima.com'
+export const baseURL = 'http://127.0.0.1:4001';
+// export const baseURL = 'https://apico.mukulima.com'
 export const defaultAvatar = 'assets/as_assets/image-efHEesPlFkfUYruMh1K4wBL3vZ7h1j0b.jpg';
 
 export const timeout = 25000;
@@ -7,8 +7,8 @@ export const X_timingdismiss = 4500;
 export const M_timingdismiss = 6000;
 export const L_timingdismiss = 7500;
 export const appprefix = "/api"; // /guard/g
-export const appname = "Mukulima ";
-export const appcompanyname = "Mukulima ETS.";
+export const appname = "APICONNECT ";
+export const appcompanyname = "Chemchem Agro";
 export const sessionKey = "_usermk";
 export const appKey = "_appID";
 export const googleAPIKey = "AIzaSyBPD1EXpbRQ4V2DegVn5OIa76FyY5JchPU";
